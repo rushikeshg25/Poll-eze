2710375311bc621edc5adf5c86a3f9c5360f2156
Rushikeshg25 <rushikeshvghotekar25@gmail.com>
2023-10-27 14:15:47 +0530

    validators for useformhook using zod for login and signup form

--- lib/validators/LoginValidator.ts
+++ lib/validators/LoginValidator.ts
@@ -1,3 +1,19 @@
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+
+export const LoginValidation =z.object(
+    {
+        username:z
+        .string()
+        .min(3, {
+          message: 'Title must be at least 3 characters long',
+        })
+        .max(128, {
+          message: 'Title must be less than 128 characters long',
+        }),
+        password: z.string().min(6, { message: "Password must be atleast 6 characters" }),
+        
+  })
+  
+
+
+  export type LoginValidationSchema = z.infer<typeof LoginValidation>;
