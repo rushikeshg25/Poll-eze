5e5df6667dc7e107cf4863407d10909a6bde72a9
Rushikeshg25 <rushikeshvghotekar25@gmail.com>
2023-11-17 23:37:52 +0530

    created model for User and Poll

---
+++ prisma/migrations/20231117092555_dev1/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Poll` (
+    `id` VARCHAR(191) NOT NULL,
+    `pollName` VARCHAR(191) NOT NULL,
+    `desc` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
