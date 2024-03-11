ee9058d0e68c391ba6a8dab8dd22c397f595801a
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-11 19:34:58 +0530

    Clerk Auth setup done

---
+++ app/api/clerk/route.ts
@@ -0,0 +1,24 @@
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { Webhook } from "svix";
+
+const webhookSecret = process.env.WEBHOOK_SECRET || ``;
+
+async function validateRequest(request: Request) {
+  const payloadString = await request.text();
+  const headerPayload = headers();
+
+  const svixHeaders = {
+    "svix-id": headerPayload.get("svix-id")!,
+    "svix-timestamp": headerPayload.get("svix-timestamp")!,
+    "svix-signature": headerPayload.get("svix-signature")!,
+  };
+  const wh = new Webhook(webhookSecret);
+  return wh.verify(payloadString, svixHeaders) as WebhookEvent;
+}
+
+export async function POST(request: Request) {
+  const payload = await validateRequest(request);
+  console.log(payload);
+  return Response.json({ message: "Received" });
+}
