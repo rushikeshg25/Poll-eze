1a01c39686e2722e6ffff29f78f4af2912b0ce72
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-12 18:34:11 +0530

    Create a new poll endpoint with test data

---
+++ app/api/poll/create-poll/route.ts
@@ -0,0 +1,67 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import { prisma } from "@/lib/db";
+
+//What a create Poll request will look like
+type PolltestT = {
+  title: string;
+  description: string;
+  Optionstest: {
+    title: string;
+  }[];
+};
+
+const PollTest: PolltestT = {
+  title: "What is the Capital of India?",
+  description: "",
+  Optionstest: [
+    { title: "Delhi" },
+    { title: "Mumbai" },
+    { title: "Nashik" },
+    { title: "Chennai" },
+  ],
+};
+
+export async function POST(Request: Request) {
+  const { userId } = auth();
+  console.log("routeHandler", userId);
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const prismaId = await prisma.user.findUnique({
+      where: {
+        externalId: userId,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (!prismaId) return {};
+    const newPoll = await prisma.poll.create({
+      data: {
+        title: PollTest.title,
+        description: PollTest.description,
+        user: {
+          connect: { externalId: prismaId.id },
+        },
+      },
+    });
+
+    PollTest.Optionstest.map(
+      async (option) =>
+        await prisma.option.create({
+          data: {
+            title: option.title,
+            optionId: newPoll.id,
+            votes: 0, // Assuming initial votes count is 0
+          },
+        })
+    );
+  } catch (error) {
+    console.log("Create Entry Error", error);
+  }
+
+  return NextResponse.json({ userId }, { status: 200 });
+}
