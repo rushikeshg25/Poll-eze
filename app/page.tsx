d8f068956b1d9d2cfcbf0c69b83329d3475e33d3
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-15 17:13:16 +0530

    Landing Page with new Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,117 +1,14 @@
 "use client";
-import { ThemeToggle } from "@/components/ui/ThemeToggle";
+import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
+import { useAuth } from "@clerk/nextjs";
 
 const Home = () => {
+  const { userId } = useAuth();
+
   return (
     <>
-      <div>
-        <header className='absolute inset-x-0 top-0 z-50 py-6'>
-          <div className='mx-auto w-full px-5 sm:px-10 md:px-12 lg:max-w-7xl lg:px-5'>
-            <nav className='relative flex w-full justify-between gap-6'>
-              <div className='relative inline-flex min-w-max'>
-                <a href='/' className='relative flex items-center gap-3'>
-                  <div className='relative flex h-7 w-7 overflow-hidden rounded-xl'>
-                    <span className='absolute -right-1 -top-1 h-4 w-4 rotate-45 rounded-md bg-green-500'></span>
-                    <span className='absolute -bottom-1 -right-1 h-4 w-4 rotate-45 rounded-md bg-[#FCDC58]'></span>
-                    <span className='absolute -bottom-1 -left-1 h-4 w-4 rotate-45 rounded-md bg-blue-600'></span>
-                    <span className='absolute left-1/2 top-1/2 h-2 w-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900'></span>
-                  </div>
-                  <div className='inline-flex text-lg font-semibold text-gray-900'>
-                    AgenceX
-                  </div>
-                </a>
-              </div>
-
-              <div
-                data-nav-overlay
-                aria-hidden='true'
-                className='fixed inset-0 hidden bg-gray-800/60 bg-opacity-50 backdrop-blur-xl backdrop-filter lg:!hidden'
-              ></div>
-              <div
-                data-navbar
-                className='invisible absolute top-full flex w-full translate-y-10 flex-col gap-x-4 gap-y-6 overflow-hidden border-x border-x-gray-100 bg-white opacity-0 duration-300 ease-linear lg:visible lg:relative lg:top-0 lg:-translate-y-0 lg:scale-y-100 lg:flex-row lg:items-center lg:justify-between lg:border-x-0 lg:!bg-transparent lg:opacity-100'
-              >
-                <ul className='flex w-full flex-col gap-x-3 gap-y-4 border-t border-gray-100 px-6 pt-6 text-lg text-gray-700 lg:flex-row lg:items-center lg:justify-center lg:border-t-0 lg:px-0 lg:pt-0'>
-                  <li>
-                    <a
-                      href='#'
-                      className='py-3 font-medium duration-300 ease-linear hover:text-blue-600'
-                    >
-                      {" "}
-                      Home{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href='#'
-                      className='py-3 font-medium duration-300 ease-linear hover:text-blue-600'
-                    >
-                      {" "}
-                      Services{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href='#'
-                      className='py-3 font-medium duration-300 ease-linear hover:text-blue-600'
-                    >
-                      {" "}
-                      About us{" "}
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href='#'
-                      className='py-3 font-medium duration-300 ease-linear hover:text-blue-600'
-                    >
-                      {" "}
-                      Features{" "}
-                    </a>
-                  </li>
-                </ul>
-
-                <div className='flex w-full items-center border-b border-gray-100 px-6 pb-6 sm:w-max lg:min-w-max lg:border-0 lg:px-0 lg:pb-0'>
-                  <a
-                    href='#'
-                    className='relative flex h-12 w-full items-center justify-center overflow-hidden rounded-full border border-transparent bg-blue-600 px-6 outline-none duration-300 ease-linear after:absolute after:inset-x-0 after:left-0 after:top-0 after:aspect-square after:origin-center after:scale-0 after:rounded-full after:bg-[#172554] after:opacity-70 after:duration-300 after:ease-linear hover:border-[#172554] hover:after:scale-[2.5] hover:after:opacity-100 sm:w-max'
-                  >
-                    <span className='relative z-10 text-white'>
-                      {" "}
-                      Get Started{" "}
-                    </span>
-                  </a>
-                  <ThemeToggle />
-                </div>
-              </div>
-
-              <div className='flex min-w-max items-center gap-x-3'>
-                <button
-                  data-toggle-navbar
-                  data-is-open='false'
-                  className='relative flex h-auto w-7 flex-col outline-none lg:invisible lg:hidden'
-                >
-                  <span
-                    id='line-1'
-                    className='h-0.5 w-6 rounded-full bg-gray-700 transition-all duration-300 ease-linear'
-                  ></span>
-                  <span
-                    id='line-2'
-                    className='rounded-ful mt-1 h-0.5 w-6 origin-center bg-gray-700 transition-all duration-300 ease-linear'
-                  ></span>
-                  <span
-                    id='line-3'
-                    className='rounded-ful mt-1 h-0.5 w-6 bg-gray-700 transition-all duration-300 ease-linear'
-                  ></span>
-                  <span className='sr-only'>togglenav</span>
-                </button>
-              </div>
-            </nav>
-          </div>
-        </header>
-      </div>
-
-      {/* Hero Section */}
+      <Navbar isLanding={true} isAuthenticated={`userId`} />
 
       <section className='relative bg-white py-32 lg:py-36'>
         <div className='mx-auto flex w-full flex-col gap-10 px-5 sm:px-10 md:px-12 lg:max-w-7xl lg:flex-row lg:gap-12 lg:px-5'>
