a0a2f376374e7b8b1127ccc2b1f85d06b7cd7a95
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-16 23:21:03 +0530

    added Navbar to signin and signup pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,13 @@
+import MainNavbar from "@/components/Navbar/MainNavbar";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className='flex h-screen justify-center items-center'>
-      <SignUp />
+    <div className='flex flex-col h-screen'>
+      <MainNavbar isAuthenticated='' isLanding={false} isAllPollsPage={false} />
+      <div className='flex-1 flex justify-center items-center '>
+        <SignUp />
+      </div>
     </div>
   );
 }
