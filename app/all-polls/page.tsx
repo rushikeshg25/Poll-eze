dfd26f25b20e3cae5f5eff6150c70ba32a07c30c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-16 20:21:27 +0530

    SSR component with All polls

--- app/all-polls/page.tsx
+++ app/all-polls/page.tsx
@@ -1,13 +1,48 @@
-"use client";
-
+import { auth } from "@clerk/nextjs";
 import MainNavbar from "@/components/Navbar/MainNavbar";
-import { useAuth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { prisma } from "@/lib/db";
+import Polls from "@/components/Data/Polls";
+import type { Poll } from "@prisma/client";
+type pollsT = { polls: Poll[] };
 
-const page = () => {
-  const { userId } = useAuth();
+async function fetchPolls(userId: string) {
+  console.log(userId);
+  try {
+    const pollsData = await prisma.user.findUnique({
+      where: {
+        externalId: userId,
+      },
+      select: {
+        polls: {
+          include: {
+            options: true,
+          },
+        },
+      },
+    });
+    const polls = pollsData?.polls;
+    return polls;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error;
+  }
+}
+
+const page = async () => {
+  const { userId } = auth();
+  console.log(userId);
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const polls = (await fetchPolls(userId)) as Poll[];
+
   return (
     <div>
       <MainNavbar isLanding={false} isAuthenticated={`userId`} />
+      <div className='flex justify-center text-2xl pt-4'>Your Polls</div>
+      <Polls polls={polls} />
     </div>
   );
 };
