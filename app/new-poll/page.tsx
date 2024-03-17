18894500f294156f31bd1af15295dd24c9479028
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-18 00:08:55 +0530

    Added Poll title and desc form with a Poll state

--- app/new-poll/page.tsx
+++ app/new-poll/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import MainNavbar from "@/components/Navbar/MainNavbar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { Progress } from "@/components/ui/progress";
@@ -11,53 +11,87 @@ import PollDuration from "@/components/CreatePollSteps/PollDuration";
 import Finalize from "@/components/CreatePollSteps/Finalize";
 import Finish from "@/components/CreatePollSteps/Finish";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
-
-const STEPS = [
-  {
-    name: "Poll Data",
-    step: 0,
-    progress: 0,
-    component: <PollData />,
-  },
-  {
-    name: "Poll Options",
-    step: 1,
-    progress: 25,
-    component: <PollOptions />,
-  },
-  {
-    name: "Poll Duration",
-    step: 2,
-    progress: 50,
-    component: <PollDuration />,
-  },
-  {
-    name: "Finalize",
-    step: 3,
-    progress: 75,
-    component: <Finalize />,
-  },
-  {
-    name: "Finish",
-    step: 4,
-    progress: 100,
-    component: <Finish />,
-  },
-];
+import type { PollT } from "@/types/PollData";
 
 const page = () => {
   const { userId } = useAuth();
-  const [currentStep, setCurrentStep] = useState<number>(0);
+
   if (!userId) {
     redirect("/sign-in");
   }
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [poll, setPoll] = useState<PollT>({
+    UserId: "",
+    description: "",
+    title: "",
+    PollType: "",
+    Options: [],
+  });
+  const [isNextDisabled, setIsNextDisabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (poll.title.length === 0) {
+      setIsNextDisabled(true);
+    } else {
+      setIsNextDisabled(false);
+    }
+  }, [poll.title]);
+
+  const descriptionhandler = (newDescription: string) => {
+    setPoll({ ...poll, description: newDescription });
+    console.log(poll);
+  };
+
+  const titlehandler = (newTitle: string) => {
+    setPoll({ ...poll, title: newTitle });
+    console.log(poll);
+  };
+
+  const STEPS = [
+    {
+      name: "Poll Data",
+      step: 0,
+      progress: 0,
+      component: (
+        <PollData
+          titlehandler={titlehandler}
+          descriptionhandler={descriptionhandler}
+        />
+      ),
+    },
+    {
+      name: "Poll Options",
+      step: 1,
+      progress: 25,
+      component: <PollOptions poll={poll} />,
+    },
+    {
+      name: "Poll Duration",
+      step: 2,
+      progress: 50,
+      component: <PollDuration poll={poll} />,
+    },
+    {
+      name: "Finalize",
+      step: 3,
+      progress: 75,
+      component: <Finalize poll={poll} />,
+    },
+    {
+      name: "Finish",
+      step: 4,
+      progress: 100,
+      component: <Finish poll={poll} />,
+    },
+  ];
 
   const onPrevious = () => {
     setCurrentStep((value) => value - 1);
   };
 
   const onNext = () => {
+    if (currentStep === 0) {
+    }
     setCurrentStep((value) => value + 1);
   };
 
@@ -87,7 +121,9 @@ const page = () => {
             {currentStep === 3 ? (
               <Button>Finish</Button>
             ) : (
-              <Button onClick={onNext}>Next</Button>
+              <Button onClick={onNext} disabled={isNextDisabled}>
+                Next
+              </Button>
             )}
           </div>
         </div>
