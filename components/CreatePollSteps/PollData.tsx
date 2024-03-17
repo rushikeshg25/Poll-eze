e511bbf7414e01ffc25887419c33056edf2b7c0a
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-18 00:08:02 +0530

    Fix:added pushing the state up to update the title and desc in main create Poll page

--- components/CreatePollSteps/PollData.tsx
+++ components/CreatePollSteps/PollData.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-const PollData = () => {
+type PollDataT = {
+  titlehandler: (a: string) => void;
+  descriptionhandler: (a: string) => void;
+};
+
+const PollData = ({ titlehandler, descriptionhandler }: PollDataT) => {
   return (
     <Card className='w-1/4 py-4 min-w-min max-w-sm'>
-      <CardHeader className='flex items-center'>
-        <CardTitle>No Polls Found!</CardTitle>
+      <CardHeader>
+        <CardTitle>Create your Poll</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className='flex justify-center w-full'></div>
+        <form>
+          <div className='grid w-full items-center gap-4'>
+            <div className='flex flex-col space-y-1.5'>
+              <Label>Title</Label>
+              <Input
+                placeholder='Title of your Poll'
+                onChange={(e) => titlehandler(e.target.value)}
+              />
+            </div>
+            <div className='flex flex-col space-y-1.5'>
+              <Label>Description</Label>
+              <Input
+                placeholder='Description of your Poll'
+                onChange={(e) => descriptionhandler(e.target.value)}
+              />
+            </div>
+          </div>
+        </form>
       </CardContent>
     </Card>
   );
