730589efd5eb5edb85fb9157d11d1922426d1127
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-18 14:02:19 +0530

    Added Select for Poll's duration and text area for description

--- components/CreatePollSteps/PollDuration.tsx
+++ components/CreatePollSteps/PollDuration.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const PollDuration = () => {
-  return <div>PollDuration</div>;
+  return (
+    <Card className='w-1/4 py-4 min-w-min max-w-sm'>
+      <CardHeader>
+        <CardTitle>Set your Poll's Duration</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className='grid w-full items-center gap-4'>
+            <div className='flex flex-col space-y-1.5'>
+              <Label>Title</Label>
+              <Input placeholder='Title of your Poll' />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default PollDuration;
--- components/CreatePollSteps/PollOptions.tsx
+++ components/CreatePollSteps/PollOptions.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const PollOptions = () => {
-  return <div>PollOptions</div>;
+  return (
+    <Card className='w-1/4 py-4 min-w-min max-w-sm'>
+      <CardHeader>
+        <CardTitle>Configure your Poll</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form id='PollDataForm'>
+          <div className='grid w-full items-center gap-4'>
+            <div className='flex flex-col space-y-1.5'>
+              <Label htmlFor='title'>Title</Label>
+              <Input id='title' placeholder='Title of your Poll' />
+            </div>
+            <div className='flex flex-col space-y-1.5'>
+              <Label htmlFor='Description'>Description</Label>
+              <Input id='Description' placeholder='Description of your Poll' />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default PollOptions;
