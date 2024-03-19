99ed625d85846d197fd77e721562d128ba4ec0e5
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-19 17:21:08 +0530

    Added Options to the Poll state in new-poll

--- components/CreatePollSteps/PollOptions.tsx
+++ components/CreatePollSteps/PollOptions.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -6,8 +7,22 @@ import { useStore } from "@/zustand/store";
 import AddTodoForm from "./PollOptionsUI/AddTodoForm";
 import Option from "./PollOptionsUI/Option";
 
-const PollOptions = () => {
-  const { options, addOption, removeOption } = useStore();
+type OptionT = {
+  title: string;
+  votes: number;
+};
+
+type PollOptionsT = {
+  addOptions: (a: OptionT[]) => void;
+};
+
+const PollOptions = ({ addOptions }: PollOptionsT) => {
+  const { options } = useStore();
+
+  useEffect(() => {
+    addOptions(options);
+  }, [options]);
+
   return (
     <CardContent className='w-full flex flex-col items-center'>
       <AddTodoForm />
