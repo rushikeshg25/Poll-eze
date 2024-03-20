b6b7aab6f96bb12b659ec5745b7837aa2a5be922
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-20 23:48:38 +0530

    Fix:Overflow issue on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -20,30 +20,35 @@ const Home = () => {
           <span className='absolute -top-5 aspect-square w-4/12 rotate-90 skew-x-12 skew-y-12 rounded-full bg-gradient-to-tr from-blue-600 to-green-400 opacity-40 blur-2xl lg:left-0 lg:w-2/12'></span>
           <div className='relative mx-auto flex max-w-3xl flex-col items-center text-center lg:mx-0 lg:w-1/2 lg:max-w-none lg:flex-1 lg:items-start lg:py-7 lg:text-left xl:py-8'>
             <div className='w-full flex justify-center'>
-              <UiImprovements />
+              <div className='p-2'>
+                <UiImprovements />
+              </div>
             </div>
 
-            <h1 className='text-3xl font-bold leading-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-6xl'>
+            <h1 className='text-3xl font-bold leading-tight dark:text-gray-400 text-gray-900 sm:text-4xl md:text-5xl xl:text-6xl'>
               Voice your opinion, Create your{" "}
               <span className='bg-gradient-to-br from-indigo-600 from-20% via-blue-600 via-30% to-green-600 bg-clip-text text-transparent'>
                 Poll
               </span>
             </h1>
-            <p className='mt-8 text-gray-700'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
-              repellat perspiciatis aspernatur quis voluptatum porro incidunt,
-              libero sequi quos eos velit
+            <p className='mt-6 text-gray-700 dark:text-gray-500'>
+              A super tool for on-the-spot feedback, online polls let you check
+              in with your audience or customers at any time. When you do, you
+              send the message that you know they’re there and their opinions
+              matter. And with instant access to poll results, you can get
+              understanding of the people you’re trying to reach and take action
+              based on their opinions, preferences.
             </p>
-            <div className='mx-auto mt-10 flex w-full max-w-md lg:mx-0'>
-              <div className='flex w-full flex-col gap-5 sm:flex-row'>
-                <form
-                  action='#'
-                  className='flex w-full items-center gap-3 rounded-full border border-gray-200 bg-gray-100 py-1 pl-6 pr-1 text-gray-600 shadow-lg shadow-gray-200/20 ease-linear focus-within:border-blue-600 focus-within:bg-white'
-                >
-                  <span className='min-w-max border-r border-gray-200 pr-2'>
+            <div className='mx-auto mt-5 flex  max-w-md lg:mx-0 w-full'>
+              <div className='flex w-full flex-col gap-5 sm:flex-row justify-center'>
+                <button className='relative flex h-12 w-max min-w-max items-center justify-center overflow-hidden rounded-full border border-transparent bg-blue-600 px-6 text-white outline-none duration-300 ease-linear after:absolute after:inset-x-0 after:left-0 after:top-0 after:aspect-square after:origin-center after:scale-0 after:rounded-full after:bg-[#172554] after:opacity-70 after:duration-300 after:ease-linear hover:border-[#172554] hover:after:scale-[2.5] hover:after:opacity-100 sm:w-max'>
+                  <span className='relative z-[5] hidden sm:flex'>
+                    {" "}
+                    Get Started{" "}
+                  </span>
+                  <span className='relative z-[5] flex sm:hidden'>
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
-                      width='20'
                       fill='none'
                       viewBox='0 0 24 24'
                       strokeWidth='1.5'
@@ -53,40 +58,11 @@ const Home = () => {
                       <path
                         strokeLinecap='round'
                         strokeLinejoin='round'
-                        d='M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z'
+                        d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
                       />
                     </svg>
                   </span>
-                  <input
-                    type='email'
-                    name=''
-                    id=''
-                    placeholder='johndoe@gmail.com'
-                    className='w-full bg-transparent py-3 outline-none'
-                  />
-                  <button className='relative flex h-12 w-max min-w-max items-center justify-center overflow-hidden rounded-full border border-transparent bg-blue-600 px-6 text-white outline-none duration-300 ease-linear after:absolute after:inset-x-0 after:left-0 after:top-0 after:aspect-square after:origin-center after:scale-0 after:rounded-full after:bg-[#172554] after:opacity-70 after:duration-300 after:ease-linear hover:border-[#172554] hover:after:scale-[2.5] hover:after:opacity-100 sm:w-max'>
-                    <span className='relative z-[5] hidden sm:flex'>
-                      {" "}
-                      Get Started{" "}
-                    </span>
-                    <span className='relative z-[5] flex sm:hidden'>
-                      <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        strokeWidth='1.5'
-                        stroke='currentColor'
-                        className='h-5 w-5'
-                      >
-                        <path
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          d='M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5'
-                        />
-                      </svg>
-                    </span>
-                  </button>
-                </form>
+                </button>
               </div>
             </div>
           </div>
