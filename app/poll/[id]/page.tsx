a6a60b4f2a742037299def4e114f7ddc80f8d0fe
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-21 18:15:06 +0530

    Added Fetch for individual poll

--- app/poll/[id]/page.tsx
+++ app/poll/[id]/page.tsx
@@ -1,3 +1,11 @@
-export default function Page({ params }: { params: { id: string } }) {
-  return <div>My Post: {params.id}</div>;
+import { fetchPoll } from "@/lib/fetchPoll";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const poll = await fetchPoll("08e3679b-64b4-4fdd-acca-a44c8fe6ef33");
+  console.log(poll);
+  return (
+    <>
+      <div>My Post: {params.id}</div>
+    </>
+  );
 }
