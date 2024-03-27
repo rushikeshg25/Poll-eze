4927c3e5656c8a1ed6f5dfb71156ea99b5bdbdc3
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-03-27 22:29:45 +0530

    create-poll's steps

--- components/CreatePollSteps/Finalize.tsx
+++ components/CreatePollSteps/Finalize.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Card, CardHeader } from "../ui/card";
+import { Card, CardDescription, CardFooter, CardHeader } from "../ui/card";
 import type { PollT } from "@/types/PollData";
+import { Votebar } from "../ui/VoteBar";
 
-const Finalize = (poll: { poll: PollT }) => {
+const Finalize = ({ poll }: { poll: PollT }) => {
   return (
-    <Card>
-      <CardHeader>{}</CardHeader>
+    <Card className='min-w-min'>
+      <CardHeader>{poll.title}</CardHeader>
+      {poll.description && (
+        <CardDescription>{poll.description}</CardDescription>
+      )}
+      <CardFooter className='flex flex-col gap-2'>
+        {poll.Options.map((option) => (
+          <Votebar
+            key={option.title}
+            option={option.title}
+            className='w-full'
+          />
+        ))}
+      </CardFooter>
     </Card>
   );
 };
