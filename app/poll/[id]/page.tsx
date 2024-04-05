d8973c5db19023cfe3cdd7c94cf919b28b975b9c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-04-05 16:34:32 +0530

    Fix:SSR works with the data fecthing

--- app/poll/[id]/page.tsx
+++ app/poll/[id]/page.tsx
@@ -1,14 +1,21 @@
+import PollPage from "@/components/Pages/PollPage";
 import MainNavbar from "@/components/Navbar/MainNavbar";
-import AuthorizedUserPoll from "@/components/Pages/AuthorizedUserPoll";
 import { fetchPoll } from "@/lib/fetchPoll";
 import type { PollwithOptionT } from "@/types/PollwithOptions";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import usePrivateHasVoted from "@/hooks/usePrivateHasVoted";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { userId } = auth();
   if (!userId) redirect(`/poll/public/${params.id}`);
   const poll = (await fetchPoll(params.id)) as PollwithOptionT;
+
+  const vote = await usePrivateHasVoted({
+    userId: userId,
+    pollId: poll.id,
+  });
+  const hasVoted = vote.hasVoted as boolean;
 
   return (
     <div className='flex flex-col w-full h-full'>
@@ -19,7 +26,7 @@ export default async function Page({ params }: { params: { id: string } }) {
       />
       <div className='flex flex-col justify-center items-center flex-1 w-full'>
         <div className='w-1/3'>
-          <AuthorizedUserPoll poll={poll} userId={userId} />
+          <PollPage poll={poll} hasVotedParent={hasVoted} />
         </div>
       </div>
     </div>
