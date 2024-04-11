0a35ff08bc4208fb87e3908fbef7011200f2b18d
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-04-12 00:28:52 +0530

    Route handler for authorized users voting

---
+++ app/api/poll/vote-poll/authorized/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+import { prisma } from "@/lib/db";
+
+export async function GET(Request: Request) {
+  const { userId } = auth();
+  if (!userId) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+  const body = await Request.json();
+  const optionId = body.optionId as string;
+  const id = body.userId as string;
+  const pollId = body.pollId as string;
+
+  try {
+    const option = await prisma.option.update({
+      where: {
+        id: optionId,
+      },
+      data: {
+        totalVotes: {
+          increment: 1,
+        },
+        votes: {
+          increment: 1,
+        },
+      },
+    });
+
+    const vote = await prisma.vote.create({
+      data: {
+        option: optionId,
+        userId: id,
+        Pvote: pollId,
+      },
+    });
+  } catch (error) {
+    console.log("Error while Voting:", error);
+  }
+
+  return NextResponse.json({ success: "Voted" }, { status: 200 });
+}
