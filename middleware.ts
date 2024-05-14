f7b12b15dcbb04d3f43f72fcab21e00ece1dbe05
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-05-14 20:03:02 +0530

    / route public

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,14 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: [
+    //Routes avaliable to public and private users
     "/",
     "/api/clerk",
     "/poll/(.*)",
     "/api/poll/vote-poll/publicuser",
   ],
 
-  ignoredRoutes: ["/", "/test", "/api/poll/vote-poll/publicuser"],
+  ignoredRoutes: ["/test", "/api/poll/vote-poll/publicuser"],
 });
 
 export const config = {
