3d38f54b7b40c0154bc4f39ce037aefb4b1fa909
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-05-29 22:12:55 +0530

    FIX:resetting polls will revalidate '/all-polls'

--- actions/ResetPoll.ts
+++ actions/ResetPoll.ts
@@ -1,5 +1,6 @@
 "use server";
 import { prisma } from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
 export const resetPoll = async (pollId: string) => {
   await prisma.poll.update({
@@ -18,4 +19,5 @@ export const resetPoll = async (pollId: string) => {
   await prisma.vote.deleteMany({
     where: { Pvote: pollId },
   });
+  revalidatePath("/all-polls");
 };
