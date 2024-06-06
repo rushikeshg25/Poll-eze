e5db133de0009ad5bc824d587e1d037deab9d87e
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-06 16:18:42 +0530

    REF:removed unused file

--- hooks/usePublicHasVoted.ts
+++
@@ -1,8 +0,0 @@
-export default function usePublicHasVoted({ pollid }: { pollid: string }) {
-  const optionId = localStorage.getItem(pollid);
-
-  if (!optionId) {
-    return null;
-  }
-  return optionId;
-}
